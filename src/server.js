6a58f7fc71e4964340b07e6b1bbd2c791dd78bab
disposable-kiwi <biffboffkev@gmail.com>
2022-09-02 12:44:32 -0400

    added login and register routes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const knex = require('knex');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+const path = require('path');
 var bodyParser = require('body-parser')
 const saltRounds = 10;
 
 const db = knex({
     client:"sqlite3",
     connection:{
-        filename: "lumbini.sqlite3"
-    }
+        filename: "../lumbini.sqlite3"
+    },
+    useNullAsDefault:true
 });
 
 const app = express();
@@ -28,10 +30,27 @@ app.post("/register", async (req,res)=>{
     }
 });
 
-app.post('/login', (res,req)=>{
-    res.json("login");
+app.post('/login', async (req,res)=>{
+    try{
+        const {email, password} = req.body;
+        const user = await db('users').first("*").where({email:email});
+        if(user){
+            const validPass = await bcrypt.compare(password, user.password);
+            if(validPass){
+                //in here is where you would send a cookie or token for authentication
+                res.status(200).json("You're logged in!")
+            }else{
+                res.json("Username and Password do not match.")
+            }
+        }else{
+            res.status(404).json("User does not exist.");
+        }
+    }catch(error){
+        console.log(error);
+        res.status(500).send("Oops, something went wrong on our end!");
+    }
 });
 
 const port = process.env.PORT || 2218;
 
-app.listen(port,()=>{console.log("Server is successfully open.")})
+app.listen(port,()=>{console.log("Server is successfully open on PORT 2218.")})
